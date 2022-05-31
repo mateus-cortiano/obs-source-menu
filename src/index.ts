b3919ea6d54c890f1a3d58b03870d26f76aaf462
mateus schwarz <mateus.cortiano@gmail.com>
2022-05-31 00:18:54 -0300

    - formatting

--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,55 @@
-import {OBSWebSocket} from "./obsws";
-import {OBSMessage} from "./util/types";
-import {wait_for_condition, wait_for} from "./util/timers"
+import { OBSWebSocket } from './obsws'
+import { OBSMessage } from './util/types'
+import { wait_for_condition, wait_for } from './util/timers'
 
-const doc: Document = document;
-const authDiv: HTMLElement = doc.getElementById("auth-div")!;
-const sceneListDiv: HTMLElement = doc.getElementById("scene-list")!;
-const authElements: { [key: string]: any } = {};
+const doc: Document = document
+const authDiv: HTMLElement = doc.getElementById('auth-div')!
+const sceneListDiv: HTMLElement = doc.getElementById('scene-list')!
+const connectBtn: HTMLElement = doc.getElementById('connect-btn')!
 
-function keyEventHandler(ev: KeyboardEvent): void {
-  switch (ev.key) {
-    case "Enter": connect(); break;
-  }
-}
+connectBtn.addEventListener('click', event => {
+  event.preventDefault()
+  const hostInput = doc.getElementById('host-input') as HTMLInputElement
+  const passInput = doc.getElementById('password-input') as HTMLInputElement
+  let host = hostInput.value as string
+  let pass = passInput.value as string
+  connect(host, pass)
+})
 
-function returnElement(attributes: { [key: string]: string }): any {
-  let el = doc.createElement(attributes["tag"]);
-  for (const attr in attributes) el.setAttribute(attr, attributes[attr]);
-  el.textContent = attributes["textContent"];
-  return el;
-}
+async function connect(host: string, pass: string): Promise<any> {
+  authDiv.className = 'fade-out-mid'
 
-async function connect(): Promise<any> {
-  authDiv.className = "fade-out-mid";
+  var ws = new OBSWebSocket(host || 'ws://localhost:4444', pass)
 
-  let host = authElements["host"].value || "ws://localhost:4444";
-  let pass = authElements["pass"].value;
-
-  var ws = new OBSWebSocket(host, pass);
-
-  await wait_for_condition(() => { return ws.isconnected; });
+  await wait_for_condition(() => {
+    return ws.isconnected
+  })
 
   if (ws.isconnected) {
-    authDiv.className = "fade-out-end";
-    await wait_for(900);
-    authDiv.remove();
+    authDiv.className = 'fade-out-end'
+    await wait_for(900)
+    authDiv.remove()
 
-    const updateButtons = async function (exclude: string=".") {
-      let response: OBSMessage = await ws.get_scene_list(exclude);
-      
-      sceneListDiv.innerHTML = "";
+    const updateButtons = async function (exclude: string = '.') {
+      let response: OBSMessage = await ws.get_scene_list(exclude)
+
+      sceneListDiv.innerHTML = ''
       response['scenes'].forEach((element: string, i: number) => {
-        let el = doc.createElement("button");
-        el.textContent = element;
-        if (i == response["active"]) el.className = "selected";
-        el.addEventListener("click", () => {
-          sceneListDiv.getElementsByClassName("selected")[0].className = "";
-          el.className = "selected";
-          ws.switch_to_scene(el.textContent as string);
-        });
-        sceneListDiv.appendChild(el);
-      });
-    };
+        let el = doc.createElement('button')
+        el.textContent = element
+        if (i == response['active']) el.className = 'selected'
+        el.addEventListener('click', () => {
+          sceneListDiv.getElementsByClassName('selected')[0].className = ''
+          el.className = 'selected'
+          ws.switch_to_scene(el.textContent as string)
+        })
+        sceneListDiv.appendChild(el)
+      })
+    }
 
-    await updateButtons();
-    ws.add_event_listener("TransitionBegin", updateButtons);
-    ws.add_event_listener("SwitchScenes", updateButtons);
-    sceneListDiv.className = "fade-in-image";
+    await updateButtons()
+    ws.add_event_listener('TransitionBegin', updateButtons)
+    ws.add_event_listener('SwitchScenes', updateButtons)
+    sceneListDiv.className = 'fade-in-image'
   }
 }
-
-function main(): void {
-  const authForm: Array<{ [key: string]: string }> = [
-    {
-      name: "host",
-      tag: "input",
-      id: "host",
-      type: "text",
-      placeholder: "host",
-      textContent: "",
-    },
-    {
-      name: "pass",
-      tag: "input",
-      id: "pass",
-      type: "password",
-      placeholder: "password",
-      textContent: "",
-    },
-    {
-      name: "button",
-      tag: "button",
-      id: "connect-btn",
-      textContent: "connect",
-    },
-  ];
-
-  authForm.forEach((el) => {
-    authElements[el["name"]] = returnElement(el);
-    authDiv.appendChild(authElements[el["name"]]);
-  });
-
-  authElements["button"].addEventListener("click", connect);
-  doc.addEventListener("keydown", keyEventHandler);
-}
-
-main();
