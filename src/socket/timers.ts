17addb054e78f67a0d523118314a83fee8d0f5b5
mateus schwarz <mateus.cortiano@gmail.com>
2022-06-01 14:54:02 -0300

    - renamed timer functions

---
+++ src/socket/timers.ts
@@ -0,0 +1,24 @@
+/* timers.ts */
+
+export async function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export async function wait_for(
+  condition: () => Boolean,
+  max_retries = 6,
+  time_mult = 5
+) {
+  async function retry(retries = 0): Promise<any> {
+    if (condition()) return Promise.resolve()
+
+    if (retries > max_retries)
+      return Promise.reject(`Max retries reached for ${condition}`)
+
+    await sleep(time_mult ** retries)
+
+    return retry(retries + 1)
+  }
+
+  return retry()
+}
