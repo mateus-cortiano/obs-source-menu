48240a49689be528719c6d33645c24b37e286278
mateus schwarz <mateus.cortiano@gmail.com>
2022-06-02 11:28:47 -0300

    - fixed creating dir

--- scripts/build.js
+++ scripts/build.js
@@ -5,12 +5,12 @@ const fs = require('fs')
 // ---
 
 const TITLE = 'obs studio scene menu'
-const OUTPUT_FILE = './public/index.html'
-// const JQUERY_CDN =
-//   '<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>'
+const OUTPUT_DIR = './public'
+const OUTPUT_FILE = 'index.html'
 
 // ---
 
+let output_path = `${OUTPUT_DIR}/${OUTPUT_FILE}`
 let buffer = `<!-- ${TITLE} -->\r\n\r\n`
 
 buffer += fs.readFileSync('./dist/index.html', 'utf8')
@@ -29,6 +29,8 @@ buffer += '\r\n</script>\r\n'
 
 buffer += '\r\n</html>\r\n'
 
-fs.writeFileSync(OUTPUT_FILE, buffer, 'utf-8', err => {
+if (!fs.existsSync(output_path)) fs.mkdirSync(OUTPUT_DIR)
+
+fs.writeFileSync(output_path, buffer, 'utf-8', err => {
   if (err) throw err
 })
